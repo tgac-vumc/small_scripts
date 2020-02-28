63465e8f71b29719a41cb531d2610d3b49027f95
Essen <h.vanessen@zeus.vumc.nl>
2020-02-28 13:54:47 +0100

    add functions getAmpUserDefinedGene

---
+++ getAmpUserDefinedGene/README.md
@@ -0,0 +1,60 @@
+# getAmpUserDefinedGene (Get Amplification of User Define Gene)
+# Hendrik F. van Essen (2020)
+
+# you need to specify the Path, 'called.Rds' file, and file with Target Genes
+
+# install.packages("QDNAseq")
+# install.packages("Biobase")
+
+### getAmpUserDefinedGene
+The *getGeneCalls* function will let the user extract all the Calls from a QDNAseq '.Rds' file with the list of Target Genes as a template.
+
+The input for *getGeneCalls* is the following:
+# calledData = QDNAseq called.Rds file
+# geneList = data frame with the following columns
+## name
+## chromosome
+## start
+
+Data will be returned from the function as a list containing the following:
+# x$sampleNames : contains the sampleNames as a list
+# x$genes : contains the data frame provide by the user
+# x$features : contains a data frame of the features from the QDNAseq object where the genes are located
+# x$calls : contains a data frame of the calls for each sample for the provided target genes
+# x$probamp : contains a data frame of the probablity store for the Amplification Calls for each sample for the provided target genes
+
+The function *getAmpUserDefinedGene* will provide the following for each sample in the data provided.
+# .csv file with target genes, calls, probamp
+# .png file for each target gene with a positive amplification call (2)
+
+The input for *getAmpUserDefinedGene* is the following:
+# calledData = QDNAseq called.Rds file
+# geneList = list structure from the function *getGeneCalls*
+# out = output path where data needs to go
+# col = user define colour of the Amplification calls
+
+### Installation
+library(QDNAseq)
+library(Biobase)
+
+load the 3 functions into R.
+# .getFeature
+# getGeneCalls
+# getAmpUserDefinedGene
+
+### PIPELINE EXAMPLE
+## set paths and file locations
+input.path <- "user defined input path"
+called.file <- file.path(path,"30kbp-called.rds" )
+gene.file <- file.path(pad, "target_genes.csv")
+output.path <- "user defined output path"
+
+# load the data 
+data <- readRDS(file = called.file)
+geneList <- read.table(file = gene.file, header = TRUE, sep = ";", skip = 0)
+
+# run the function
+getAmpUserDefinedGene(calledData = data, geneList = geneList, out = output.path, col = "lightblue")
+
+### Citation
+Not available
---
+++ getAmpUserDefinedGene/getAmpUserDefinedGene.R
@@ -0,0 +1,91 @@
+### load libraries ----
+library(QDNAseq)
+library(Biobase)
+
+#### PATH AND FILE NAMES ----
+pad <- "\\\\vumc.nl\\home$\\store4ever\\h.vanessen\\h.vanessen\\Desktop\\UMCG"
+file <- file.path(pad,"CNP20-003_30kbp-called.rds" )
+manifest <- file.path(pad, "target_genes.csv")
+
+#### PATH AND FILE NAMES ----
+input.path <- "user defined input path"
+called.file <- file.path(path,"30kbp-called.rds" )
+gene.file <- file.path(pad, "target_genes.csv")
+output.path <- "user defined output path"
+
+### LOAD DATA ----
+data <- readRDS(file = file)
+geneList <- read.table(file = manifest, header = TRUE, sep = ";", skip = 0)
+
+### FUNCTIONS ----
+.getFeature <- function(calledData, chromosome, position){
+  x <- data@featureData@data$chromosome == chromosome & 
+             position >= data@featureData@data$start &
+             position <= data@featureData@data$end 
+  return(x)
+}
+getGeneCalls <- function(calledData, geneList){
+  colnames(geneList) <- tolower(colnames(geneList))
+  header <- c("name","chromosome","start")
+  if(sum(header %in% colnames(geneList)) != 3){
+    stop("GeneList does not contain the required columns.\n  * name\n  * chromosome\n  * start\n\n ")
+  }
+  nSamples <- length(sampleNames(data))
+  results.feature <- matrix(NA, nrow = nrow(geneList), ncol = 3)
+  colnames(results.feature) <- c("Chromosome","Start", "End")
+  results.calls <- matrix(NA, nrow = nrow(geneList), ncol = nSamples)
+  colnames(results.calls) <- sampleNames(data)
+  results.probamp <- results.calls
+  sel <- rep(FALSE, nrow(geneList))
+  for(gene in 1:nrow(geneList)){
+    feature <- .getFeature(calledData = data, chromosome = geneList$chromosome[gene], position = geneList$start[gene])
+    results.feature[gene,] <- unlist(data@featureData@data[feature,][1:3])
+    results.calls[gene,] <- data@assayData$calls[feature,]
+    results.probamp[gene,] <- data@assayData$probamp[feature,]
+  }
+  rownames(results.feature) <- rownames(results.calls) <- rownames(results.probamp) <- geneList$name
+  results <- list(sampleNames = sampleNames(data),
+                  genes = geneList,
+                  features = results.feature,
+                  calls = results.calls,
+                  probamp = results.probamp)
+  return(results)
+}
+getAmpUserDefinedGene <- function(calledData, geneList, out, col = "green"){
+  cat(as.character(Sys.time()), ": Extracting gene data.\n")
+  geneData <- getGeneCalls(calledData = data, geneList = geneList)
+  for(i in 1:nSamples){
+    cat(as.character(Sys.time()), ": Processing sample -", sampleNames(data)[i],"\n")
+    sel <- geneData$calls[,i] %in% 2
+    sampleData <- cbind(geneData$genes[,1:2],
+                        geneData$features,
+                        calls = geneData$calls[,i],
+                        probamp = geneData$probamp[,i])
+    
+    output.file <- file.path(out, paste(geneData$sampleNames[i], "_results.csv", sep = ""))
+    write.table(x = sampleData, file = output.file, quote = FALSgenE, sep = ";", row.names = FALSE)
+    if(TRUE %in% sel == TRUE){
+      cat(as.character(Sys.time()), ": Plotting amplifications.\n")
+      for(g in 1:nrow(geneData$genes)){
+        if(geneData$calls[g,i] == 2){
+          output.plot <- paste(geneData$sampleNames[i],"_",
+                               geneData$genes[g,1],"_",
+                               paste("chr",geneData$features[g,1],"_",geneData$features[g,2],"-",geneData$features[g,3], sep = ""),
+                               ".png", sep = "")
+          output.plot <- file.path(out, output.plot)
+          png(filename = output.plot, width = 1500, height = 750)
+          plot(data[chromosomes(data) == geneData$features[g,1],i], gaincol = FALSE, losscol = FALSE, delcol = FALSE, ampcol = col)
+          mtext(text = rownames(geneData$features)[g], at = geneData$features[g,2], padj = -1, cex = 0.8)
+          dev.off()
+        }
+      }
+    } else {
+      cat(as.character(Sys.time()), ": No Amplication calls detected.\n")
+    }
+  }
+}
+
+### RUN THE CODE ----
+getAmpUserDefinedGene(calledData = data, geneList = geneList, out = pad, col = "lightblue")
+
+### END ----
